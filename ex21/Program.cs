00cfb6adee2b77cd277ee36ccf425513acd70a55
olegterentiev69 <135621672+olegterentiev69@users.noreply.github.com>
2023-06-21 15:50:51 +0300

    Задача 21

--- ex21/Program.cs
+++ ex21/Program.cs
@@ -1,16 +1,16 @@
 ﻿Console.Clear();
  
- Console.Write("Введите x1: ");
+ Console.Write("Введите x точки A: ");
  double x1 = double.Parse(Console.ReadLine() ?? "");
- Console.Write("Введите x2: ");
+ Console.Write("Введите x точки B: ");
  double x2 = double.Parse(Console.ReadLine() ?? "");
- Console.Write("Введите y1: ");
+ Console.Write("Введите y точки A: ");
  double y1 = double.Parse(Console.ReadLine() ?? "");
- Console.Write("Введите y2: ");
+ Console.Write("Введите y точки B: ");
  double y2 = double.Parse(Console.ReadLine() ?? "");
- Console.Write("Введите z1: ");
+ Console.Write("Введите z точки A: ");
  double z1 = double.Parse(Console.ReadLine() ?? "");
- Console.Write("Введите z2: ");
+ Console.Write("Введите z точки B: ");
  double z2 = double.Parse(Console.ReadLine() ?? "");
 
 double distance = Math.Sqrt((x2-x1)*(x2-x1)+(y1-y2)*(y2-y1)+(z2 - z1)*(z2-z1));

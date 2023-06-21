fcd370aac32f9af98b5e1af4e7c09f3919a39f15
olegterentiev69 <135621672+olegterentiev69@users.noreply.github.com>
2023-06-21 16:48:49 +0300

    Задача 23

--- ex23/Program.cs
+++ ex23/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Программа на вход получает число и выдаёт таблицу кубов чисел от 1 до этого числа.
+
+Console.Clear();
+Console.Write("Введите целое число: ");
+int a = int.Parse(Console.ReadLine());
+int b = 0;
+
+Console.Write($"{a} -->");
+while(b != a-1)
+{
+    b++;
+Console.Write($" {b*b*b}, ");
+}
+b++;
+Console.Write($" {b*b*b}. ");

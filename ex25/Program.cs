a26a0e62bdcf9fbb0cb4405b583eaa362a94d5a5
olegterentiev69 <135621672+olegterentiev69@users.noreply.github.com>
2023-06-26 14:03:58 +0300

    Задача 25

---
+++ ex25/Program.cs
@@ -0,0 +1,31 @@
+﻿int Prompt(string message)
+{
+    System.Console.Write(message);
+    string readInput = System.Console.ReadLine();
+    int result = int.Parse(readInput);
+    return result;
+}
+int Power(int powerBase, int exponent)
+{
+    int power = 1;
+    for (int = 0; i < exponent; i++)
+    {
+        power *= powerBase;
+    }
+    return power;
+}
+bool ValidateExponent(int exponent)
+{
+    if (exponent < 0)
+    {
+        System.Console.Writeline("Показатель не должен быть меньше нуля");
+        return false;
+    }
+    return true;
+}
+int powerBase = Prompt("Введите основание: ");
+int exponent = Prompt(" Введите показатель степени: ");
+if (ValidateExponent(exponent))
+{
+    System.Console.Writeline($"Число(powerbase) в степени(exponent) равно {Power(powerBase, exponent)}");
+}

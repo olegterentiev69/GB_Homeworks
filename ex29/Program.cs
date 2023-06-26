4a5a40e2b75792569efb555a11e2eb041712fa33
olegterentiev69 <135621672+olegterentiev69@users.noreply.github.com>
2023-06-26 15:50:46 +0300

    Задача 29

---
+++ ex29/Program.cs
@@ -0,0 +1,34 @@
+﻿int Prompt (string message)
+   {
+       System.Console.Write(message);
+       string readInput = System.Console.ReadLine();
+       int result = int.Parse(readInput);
+       return result;
+   }
+   int [] GenerateArray(int Length, int minValue, int maxValue)
+   {
+       int[] array = new int[Length];
+       Random random = new Random();
+       for (int = 0, i < Length; i++);
+       {
+           array[i] = random.Next(minValue, maxValue + 1);
+       }
+       
+      return array;
+   }
+   void Printarray(int[] array)
+   {
+       
+       System.Console.Write("[");
+       for (int i = 0; i < array.Length - 1; i++)
+       {
+           System.Console.Write($" {array[i]}, ");
+       }
+       System.Console.Write($" {array[array.Length - 1]},");
+       System.Console.WriteLine("]");
+   }
+   int length = Prompt ("Длина массива: ");
+   int min = Prompt (" Начальное значение для диапазона случайного числа: ");
+   int max = Prompt (" Конечное значение для диапазона случайного числа: ");
+   int[] array = GenerateArray(length, min, max);
+   Printarray(array);

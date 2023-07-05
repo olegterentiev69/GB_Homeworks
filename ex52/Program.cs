afd965c8691fc92f858de07ec5ee0d0ae8a78adc
olegterentiev69 <135621672+olegterentiev69@users.noreply.github.com>
2023-07-05 16:45:06 +0300

    Задача 52

---
+++ ex52/Program.cs
@@ -0,0 +1,58 @@
+﻿//Задайте двумерный массив размером m×n, 
+//заполненный случайными целыми числами
+Console.Clear();
+Console.Write("Введите количество строк массива: ");
+int rows = int.Parse(Console.ReadLine() ?? "");
+Console.Write("Введите количество столбцов массива: ");
+int columns = int.Parse(Console.ReadLine() ?? "");
+int minValue = GetNumberFromUser("Введите минимальное значение массива: ", "Ошибка ввода!");
+int maxValue = GetNumberFromUser("Введите максимальное значение массива: ", "Ошибка ввода!");
+
+int[,] array = GetArray(rows, columns, minValue, maxValue);
+PrintArray(array);
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+int GetNumberFromUser(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if (isCorrect)
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
+Console.WriteLine("---------------------------");
+for (int j = 0; j < array.GetLength(1); j++)
+{
+    double sum = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        sum += array[i, j];
+    }
+    Console.Write($"{sum / array.GetLength(0)} ");
+}
+Console.ReadLine();

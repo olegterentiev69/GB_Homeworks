c9e98817937023aee00d681536cf3de06569e04b
olegterentiev69 <135621672+olegterentiev69@users.noreply.github.com>
2023-07-07 16:40:44 +0300

    Задача 54

---
+++ ex54/Program.cs
@@ -0,0 +1,50 @@
+﻿Console.Clear();
+int[,] matrix = new int[5, 6];
+FillArray(matrix);
+Console.WriteLine();
+PrintArray(matrix);
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]}\t ");
+        }
+        Console.WriteLine();
+    }
+}
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(-5, 11);//[1; 10)
+        }
+    }
+}
+//
+void uporyadok(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+{
+    if (array[i, k] < array[i, k + 1])
+    {
+        int temp = array[i, k + 1];
+        array[i, k + 1] = array[i, k];
+        array[i, k] = temp;
+    }
+}
+        }
+        }
+        }
+uporyadok(matrix);
+Console.WriteLine("Отсортированный массив: ");
+PrintArray(matrix);
+

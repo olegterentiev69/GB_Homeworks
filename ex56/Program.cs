8431c8b4dfff3fa81beebb97aeccdadf15467847
olegterentiev69 <135621672+olegterentiev69@users.noreply.github.com>
2023-07-10 17:14:51 +0300

    Задача 56

--- ex56/Program.cs
+++ ex56/Program.cs
@@ -24,30 +24,30 @@ void FillArray(int[,] matr)
     {
         for (int j = 0; j < matr.GetLength(1); j++)
         {
-            matr[i, j] = new Random().Next(-5, 11);//[1; 10)
+            matr[i, j] = new Random().Next(-5, 11);
         }
     }
 }
 // Функция, считающая сумму элементов в строке
 int SumLine(int[,] array, int i)
 {
-int sum = array[i,0];
-for (int j = 1; j < array.GetLength(1); j++)
-{
-sum += array[i,j];
-}
-return sum;
+    int sum = array[i, 0];
+    for (int j = 1; j < array.GetLength(1); j++)
+    {
+        sum += array[i, j];
+    }
+    return sum;
 }
 
 int minSum = 1;
 int sum = SumLine(matrix, 0);
 for (int i = 1; i < matrix.GetLength(0); i++)
 {
-if (sum > SumLine(matrix, i))
-{
-sum = SumLine(matrix, i);
-minSum = i+1;
-}
+    if (sum > SumLine(matrix, i))
+    {
+        sum = SumLine(matrix, i);
+        minSum = i + 1;
+    }
 }
 Console.WriteLine("-------------------------------------------");
 Console.WriteLine($"Строка c наименьшей суммой элементов: № {minSum}");

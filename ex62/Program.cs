204a7b045964b403bdf3a3fdf154bcc208898506
olegterentiev69 <135621672+olegterentiev69@users.noreply.github.com>
2023-07-10 13:13:14 +0300

    Задача 62

---
+++ ex62/Program.cs
@@ -0,0 +1,41 @@
+﻿//Напишите программу, которая заполнит спирально массив 4 на 4.
+
+Console.Clear();
+int n = 4;
+int[,] squareMatrix = new int[n, n];
+
+int temp = 1;
+int i = 0;
+int j = 0;
+
+while (temp <= squareMatrix.GetLength(0) * squareMatrix.GetLength(1))
+{
+  squareMatrix[i, j] = temp;
+  temp++;
+  if (i <= j + 1 && i + j < squareMatrix.GetLength(1) - 1)
+    j++;
+  else if (i < j && i + j >= squareMatrix.GetLength(0) - 1)
+    i++;
+  else if (i >= j && i + j > squareMatrix.GetLength(1) - 1)
+    j--;
+  else
+    i--;
+}
+
+PrintArray(squareMatrix);
+
+void PrintArray (int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      if (array[i,j] / 10 <= 0)
+      Console.Write($" {array[i,j]} ");
+
+      else Console.Write($"{array[i,j]} ");
+    }
+    Console.WriteLine();
+  }
+}
+  

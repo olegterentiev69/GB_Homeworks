4ebb9aa6999cf4c7b40fad8316658641e948522c
olegterentiev69 <135621672+olegterentiev69@users.noreply.github.com>
2023-07-13 13:17:13 +0300

    Задача 64

---
+++ ex64/Program.cs
@@ -0,0 +1,15 @@
+﻿//Задайте значение N. Напишите программу, которая выведет все
+//натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+Console.Clear();
+Console.Write("Введите натуральное число больше 1: -> ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("-------------------------------------------");
+GetNum(number);
+void GetNum(int number)
+{
+    if (number <= 0) return;
+    Console.Write("{0,3}", number);
+    GetNum(number - 1);
+}
+

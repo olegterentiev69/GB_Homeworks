38071bba2db7ba4c129917e4e04fba10715d3c92
olegterentiev69 <135621672+olegterentiev69@users.noreply.github.com>
2023-07-13 13:19:04 +0300

    Задача 66

---
+++ ex66/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задайте значения M и N. Напишите программу, которая найдёт сумму 
+//натуральных элементов в промежутке от M до N.
+
+Console.Clear();
+Console.Write("Введите значение M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int summa(int m, int n)
+{
+    if (m == n)
+        return n;
+    else return m + summa(m + 1, n);
+}
+Console.WriteLine("------------------------------------------------------------------");
+Console.Write($"Сумма натуральных элементов в промежутке от M до N равна {summa(m, n)}");
